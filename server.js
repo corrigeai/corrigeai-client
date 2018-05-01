cebf4be7997d1756fca153fd62104a32730f009a
Benardi <benardinunes@gmail.com>
2018-05-01 11:23:31 -0300

    Improve server script. Provide favicon on deploy.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const app = express();
 
 // If an incoming request uses
@@ -8,22 +9,34 @@ const app = express();
 // redirect that request to the
 // same url but with HTTPS
 const forceSSL = function() {
-  return function (req, res, next) {
+  return function(req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
+        ['https://', req.get('Host'), req.url].join('')
       );
     }
     next();
   }
-}
+};
+
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+//app.use(forceSSL());
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
+/*
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+*/
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 4200, function() {
+  console.log('Listening on port ' + this.address().port); //Listening on port 8888
 });
