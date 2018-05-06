d8ff4a57678e507d8a20143bbcca7bd82ff9e942
Benardi <benardinunes@gmail.com>
2018-05-06 19:35:21 -0300

    Add TS Models to enforce response body. Catch error on http request.

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
 
+import {EditUserBody, LoginBody, RegistrationBody} from '../models/body-obj.model';
+
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class AuthenticationService {
@@ -12,17 +15,19 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  signUp(userData: any): Observable<Boolean> {
+  signUp(userData: RegistrationBody): Observable<Boolean> {
     return this.http.post(this.API.concat('/user'), userData)
     .map((res: Response) => {
       if (res) {
         return true;
       }
       return false;
+    }).catch((error: Response) => {
+      return  Observable.throw(error.json());
     });
   }
 
-  login(userData: any): Observable<Boolean> {
+  login(userData: LoginBody): Observable<Boolean> {
     return this.http.post(this.API.concat('/auth'), userData)
     .map((res: Response) => {
       if (res) {
@@ -30,6 +35,20 @@ export class AuthenticationService {
         return true;
       }
       return false;
-    });
+    }).catch((error: Response) => {
+        return  Observable.throw(error.json());
+      });
+  }
+
+  editUser(userData: EditUserBody): Observable<Boolean> {
+    return this.http.post(this.API.concat('/edit-user'), userData)
+      .map((res: Response) => {
+        if (res) {
+          return true;
+        }
+        return false;
+      }).catch((error: Response) => {
+        return  Observable.throw(error.json());
+      });
   }
 }
