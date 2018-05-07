8e1e7361cc076bc2c5daacefc3ec45544ed7cdc9
Benardi <benardinunes@gmail.com>
2018-05-07 12:50:45 -0300

    Order imports. Delete unused env variables

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { environment } from '../../environments/environment';
 
-import {EditUserBody, LoginBody, RegistrationBody} from '../models/body-obj.model';
+import {LoginBody, RegistrationBody} from '../models/body-obj.model';
+import { environment } from '../../environments/environment';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -37,18 +37,6 @@ export class AuthenticationService {
       }
       return false;
     }).catch((error: Response) => {
-        return  Observable.throw(error.json());
-      });
-  }
-
-  editUser(userData: EditUserBody): Observable<Boolean> {
-    return this.http.post(this.API.concat('/edit-user'), userData)
-      .map((res: Response) => {
-        if (res) {
-          return true;
-        }
-        return false;
-      }).catch((error: Response) => {
         return  Observable.throw(error.json());
       });
   }
