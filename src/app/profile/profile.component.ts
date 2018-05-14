d8eca8ac5688f2f98a6f5517ba155761da8e7a80
Benardi <benardinunes@gmail.com>
2018-05-14 09:54:11 -0300

    Link profile properties to LocalStorage

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  name: String;
+  gender: String;
+  email: String;
+  username: String;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    this.name = userInfo.name;
+    this.gender = userInfo.gender;
+    this.email = userInfo.email;
+    this.username = userInfo.username;
   }
 
 }
