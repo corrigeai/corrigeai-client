60ab6bdfbeade79420ae56c348511677cee6eb0e
Benardi <benardinunes@gmail.com>
2018-05-14 20:06:11 -0300

    Put dummy content on profile page. Change text color to silver

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,19 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  name: String;
-  gender: String;
-  email: String;
-  username: String;
+  name: String = "Dummy name";
+  gender: String = "Unknown";
+  email: String = "test@test.com";
+  username: String = "dummy_user";
 
   constructor() {}
 
   ngOnInit() {
-    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-    this.name = userInfo.name;
-    this.gender = userInfo.gender;
-    this.email = userInfo.email;
-    this.username = userInfo.username;
+  
+    if (localStorage.getItem('userInfo') !== null) {
+      const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+      
+      this.name = userInfo.name;
+      this.gender = userInfo.gender;
+      this.email = userInfo.email;
+      this.username = userInfo.username;  
+    }
   }
 
 }
