34b41b0f544024c39779990afad1525556b5cab2
Ravi Leite <ravi.lopes@ccc.ufcg.edu.br>
2018-06-11 03:48:43 -0300

    HomePage and NavBar almost done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { AuthenticationService } from '../services/authentication.service';
+import {isUndefined} from "util";
 
 @Component({
   selector: 'app-header',
@@ -9,12 +9,36 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  show = true;
+  inHome = false;
 
   constructor(private router: Router,
-    private authService: AuthenticationService) {}
+    private authService: AuthenticationService) {
+  }
 
   onLogout (): void {
+    this.show = true;
     this.authService.logOut();
     this.router.navigate(['/']);
+  }
+
+  isLogged(){
+    if(isUndefined(localStorage.getItem('token')) || localStorage.getItem('token') != null) {
+      this.show = false;
+      return false;
+    }else{
+      this.show = true;
+      return true;
+    }
+  }
+
+  redirect(){
+    this.isLogged();
+    if(!this.show){
+      this.router.navigate(['/home-page']);
+      this.inHome = true;
+    }else{
+      this.router.navigate(['/']);
+    }
   }
 }
