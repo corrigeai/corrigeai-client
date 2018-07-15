c74ad197c0fe6d4cf4c1aea25770ef86dc4b1516
Benardi <benardinunes@gmail.com>
2018-07-15 20:52:45 -0300

    Add badges related service

---
+++ src/app/services/badges.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+
+import { AuthenticationService } from './authentication.service';
+import { environment } from '../../environments/environment';
+import { ErrorService } from './error.service';
+
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class BadgesService {
+
+    /** Production/Development API URL */
+    API = environment.apiUrl;
+
+    constructor(private http: HttpClient,
+        private authService: AuthenticationService,
+        private errorService: ErrorService) {}
+
+    /**
+     * Requests the badges of a user.
+     * @param userId - Requesting user id.
+     */
+    getBadgesByUserId(userId: string): Observable<any> {
+        const httpOptions = this.authService.getOptions();
+        return this.http.get(this.API.concat('badges/' + userId), httpOptions)
+        .map((response: Response) => response)
+        .catch((error: Response) => {
+            this.errorService.handleError(error);
+            return  Observable.throw(error);
+          });
+    }
+}
