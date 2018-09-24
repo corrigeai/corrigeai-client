223e8c90d41ddefd18b9a91d9ef8b93a634a8c54
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-09-24 18:56:32 -0300

    Add first graph.

--- src/app/charts/rating-evolution/rating-evolution.component.ts
+++ src/app/charts/rating-evolution/rating-evolution.component.ts
@@ -33,15 +33,53 @@ export class RatingEvolutionComponent implements OnInit, OnChanges {
   }
 
   generateChart() {
+    const labels = this.createIndexes(this.data.length);
+    const colors = labels.map(i => this.intToRGB(i));
+
     this.chart = new Chart('canvas', {
       type: 'line',
       data: {
         datasets: [{
-          label: 'Evolução das suas notas',
-          data: this.data
-        }]
+          data: this.data,
+          backgroundColor: colors,
+          fill: false
+        }],
+        labels: labels
+      },
+      options: {
+        title: {
+          display: true,
+          text: ['Evolução das suas notas.'],
+        },
+
+        scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true,
+                  max: 1000
+              },
+              scaleLabel: {
+                  display: true,
+                  labelString: 'Média da avaliação'
+              }
+          }]
+        }
       }
     });
+  };
+
+  private createIndexes(length) {
+    return Array(length).fill(undefined).map((_, idx) => idx.toString());
+  }
+
+  private intToRGB(value) {
+    value = parseInt(value) * 1000 * Math.random();
+    
+    const blue = Math.floor(value % 256);
+    const green = Math.floor(value / 256 % 256);
+    const red = Math.floor(value / 256 / 256 % 256);
+
+    return "rgb(" + red + "," + green + "," + blue + ")";
   }
 
 }
